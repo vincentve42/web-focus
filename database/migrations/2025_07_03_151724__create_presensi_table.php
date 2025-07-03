653f0c450bd363eb9a719f13c704ef1b871ae4cf
vincentve42 <vincentve42@gmail.com>
2025-07-03 22:52:34 +0700

    Database

--- database/migrations/2025_07_03_151724__create_presensi_table.php
+++ database/migrations/2025_07_03_151724__create_presensi_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer('status')->default(0);
+            $table->text('image')->default('');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
         });
     }
 
