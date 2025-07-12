f45519971839516e11b2af83b655b78e5aed3285
Vincent Vernando Ekklesia <160295389+vincentve42@users.noreply.github.com>
2025-07-12 08:56:09 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -27,53 +27,34 @@ Route::middleware([LoggedInCheckerAsUser::class])->group(function (){
 });
 Route::middleware([AdminChecker::class])->group(function (){
     // Dashboard
-
     Route::get('/admin/dashboard',[AdminController::class,'DashboardUi'])->name('DashboardUi');
-    
     // Absen / Presensi
-
     Route::get('/admin/presensi',[AdminController::class,'PresensiUi'])->name('PresensiUi');
     Route::get('/admin/presensi-user',[AdminController::class,'PresensiAllUi'])->name('PresensiAllUi');
      Route::get('/admin/presensi-view/{id}',[AdminController::class,'ViewPresensi'])->name('ViewPresensi');
-   
     Route::get('/admin/presensi/delete/{id}',[AdminController::class,'DeleteAbsen'])->name('DeleteAbsen');
     Route::get('/admin/presensi/confirm/{id}',[AdminController::class,'ConfirmAbsen'])->name('ConfirmAbsen');
-
     // Laporan_Keuangan
-
     Route::get('/admin/laporan-keuangan',[AdminController::class,'LaporanKeuanganUi'])->name('LaporanKeuanganUi');
     Route::post('/admin/laporan-keuangan',[AdminController::class,'AddKeuangan'])->name('AddKeuangan');
     Route::get('/admin/laporan-keuangan/delete/{id}',[AdminController::class,'DeleteLaporanKeuangan'])->name('DeleteLaporanKeuangan');
     Route::get('/admin/editkeuangan/{id}',[AdminController::class,'EditLaporanKeuanganUi'])->name('EditLaporanKeuanganUi');
     Route::post('/admin/editkeuangan/{id}',[AdminController::class,'EditLaporanKeuangan'])->name('EditKeuangan');
-
     // Page
-
     Route::get('/admin/next/{panel}',[AdminController::class,'NextPage'])->name('NextPage');
     Route::get('/admin/back/{panel}',[AdminController::class,'BackPage'])->name('BackPage');
-
     // Dokumentasi Panel
-    
     Route::get('admin/invite-user',[AdminController::class, 'InviteDokumUi'])->name('InviteDokumUi');
     Route::post('admin/invite-user',[AdminController::class, 'RewardUser'])->name('RewardUser');
-    Route::get('admin/invite/{id}',[AdminController::class, 'InviteDokum'])->name('InviteDokum');
-
+    Route::get('admin/invite/{id}',[AdminController::class, 'InviteDokum'])->name('InviteDokum')
     // Kas
-
     Route::get('admin/user-kas',[AdminController::class, 'ShowUserKasUi'])->name('ShowUserKasUi');
     Route::get('admin/edit-kas/{id}',[AdminController::class, 'ShowKasUser'])->name('ShowKasUser');
     Route::post('admin/edit-kas/{id}',[AdminController::class, 'EditKasUser'])->name('EditKasUser');
-
     // nOTIF
-
     Route::get('admin/notif', [AdminController::class,'NotifUi'])->name('NotifUi');
-
     // Search
-
-    Route::post('admin/search/{panel}',[AdminController::class, 'Search'])->name('Search');
-
-    
-    
+    Route::post('admin/search/{panel}',[AdminController::class, 'Search'])->name('Search'); 
 });
 
 Route::get('chat', [ChatController::class,'ChatUi'])->name('ChatUi')->middleware([ChatChecker::class]);
